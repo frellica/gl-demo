5c4bf326e375e6d4965a97aa7561d1b61555c773
frellica <glbrt.liu@gmail.com>
2016-01-28 17:24:15 +0800

    FEAT: demo-1

--- gilgl/main.cpp
+++ gilgl/main.cpp
@@ -6,10 +6,37 @@
 //  Copyright © 2016年 frellica. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+#include <glfw/glfw3.h>
+
+#include <OpenGL/gl.h>
+int main(int argc, char ** argv) {
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow *window;
+    window = glfwCreateWindow(1024, 768, "gl demo 1", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    do {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+             glfwWindowShouldClose(window) == 0);
+    
+    
 }
