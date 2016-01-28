ce8a170dcc0ea3cdbf6d3cadc54cbdb2e9ad3b88
frellica <glbrt.liu@gmail.com>
2016-01-28 14:33:43 +0800

    Initial Commit

---
+++ gilgl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gilgl
+//
+//  Created by frellica on 16/1/28.
+//  Copyright © 2016年 frellica. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
